58022c019cd3eab37f8170e9a25d25f46949e7a0
Kamalesh S <73590020+kamalss11@users.noreply.github.com>
2021-07-27 17:48:41 +0530

    Add files via upload

---
+++ JS/js.js
@@ -0,0 +1,19 @@
+var fname = document.querySelector("#fn")
+var lname = document.querySelector("#ln")
+var phnum = document.querySelector("#ph")
+var email = document.querySelector("#ema")
+var password = document.querySelector("#pss")
+var er = document.querySelector(".er")
+
+phnum.addEventListener("blur",function(){
+    let ph = /\d[0-9]$/
+    if(fname.value.length > 10){
+        er.classList.add("active")
+    }
+    else if(!phnum.value.match(ph)){
+        er.classList.add("active")
+    }
+    else{
+        er.classList.remove("active")
+    }
+})
